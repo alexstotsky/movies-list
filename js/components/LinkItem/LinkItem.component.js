1f83658dc79b1721ef9892ef43b24d496d1967af
rverbytskyi <r.j.verbytskyi@gmail.com>
2019-01-26 17:18:48 +0200

    Add InAppBrowser

--- js/components/LinkItem/LinkItem.component.js
+++ js/components/LinkItem/LinkItem.component.js
@@ -1,36 +1,57 @@
 import React from 'react'
-import { Text, TouchableOpacity } from 'react-native'
+import { Text, TouchableOpacity, Linking } from 'react-native'
 import PropTypes from 'prop-types'
+import InAppBrowser from 'react-native-inappbrowser-reborn'
 
 import { styleSheet } from '../../utils/commonPropTypes'
 
 class LinkItem extends React.PureComponent {
   static propTypes = {
-    title: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
     link: PropTypes.string.isRequired,
     containerStyle: styleSheet,
-    subtitleStyle: styleSheet,
+    labelStyle: styleSheet,
   }
 
   static defaultProps = {
-    subtitle: '',
+    title: '',
   }
 
-  onPress = (link) => {
-    console.log(link)
+  onPress = async (link) => {
+    const isAvailable = await InAppBrowser.isAvailable()
+    if (isAvailable) {
+      InAppBrowser.open(link, {
+        // iOS Properties
+        dismissButtonStyle: 'cancel',
+        preferredBarTintColor: '#dadada',
+        preferredControlTintColor: '#273236',
+        readerMode: false,
+        // Android Properties
+        showTitle: true,
+        toolbarColor: '#273236',
+        secondaryToolbarColor: '#273236',
+        enableUrlBarHiding: true,
+        enableDefaultShare: true,
+        forceCloseOnRedirection: false,
+      })
+        .then(() => {})
+        .catch(err => console.warn('[InAppBrowser] returned following error:\n', err))
+    } else {
+      Linking.openURL(link)
+    }
   }
 
   render() {
     const {
-      title, link, containerStyle, subtitleStyle,
+      label, link, containerStyle, labelStyle,
     } = this.props
     return (
       <TouchableOpacity
         style={containerStyle}
         onPress={() => this.onPress(link)}
       >
-        <Text style={subtitleStyle}>
-          {title}
+        <Text style={labelStyle}>
+          {label}
         </Text>
       </TouchableOpacity>
     )
